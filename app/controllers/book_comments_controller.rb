24e5265707fbc8adc868d187975720c41b190a6e
Raychel-310 <wakimoto.reina.infratop@gmail.com>
2023-10-21 07:22:40 +0000

    応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,15 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = book.id
-    @comment.save
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = book.id
+    @book_comment.save
     #redirect_to book_path(book.id)
   end
 
   def destroy
-    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    @comment.destroy
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment.destroy
     #redirect_to book_path(book.id)
   end
 
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,19 @@
 class FavoritesController < ApplicationController
 
   def create
-    if !Favorite.exists?(book_id: params[:book_id], user_id: current_user.id)
+    #if !Favorite.exists?(book_id: params[:book_id], user_id: current_user.id)
       @book = Book.find(params[:book_id])
       @favorite = current_user.favorites.new(book_id: @book.id)
       @favorite.save
       #redirect_to 'favorites/create'
-    end
+    #end
   end
 
   def destroy
-    if Favorite.exists?(book_id: params[:book_id], user_id: current_user.id)
+    #if Favorite.exists?(book_id: params[:book_id], user_id: current_user.id)
       @book = Book.find(params[:book_id])
       @favorite = current_user.favorites.find_by(book_id: @book.id)
       @favorite.destroy
-    end
+    #end
   end
 end
